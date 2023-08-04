afb5e253452fa00709ffe0593a5ffd9a028eb6b4
sreekar-panchangam <sreekarpanchangam@gmail.com>
2023-08-04 21:55:16 +0530

    ID 2

--- workorder/urls.py
+++ workorder/urls.py
@@ -5,4 +5,5 @@ app_name = 'workorder'
 
 urlpatterns = [
     path("search/", views.HoistWorkOrderListView.as_view(), name="hwo_search"),
+    path("<int:hwo>/", views.HoistWorkOrderDetailView.as_view(), name="hwo_detail"),
 ]
--- workorder/views.py
+++ workorder/views.py
@@ -1,61 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework.views import APIView
 from django.db.models import Q
 from .models import HoistWorkOrder
 from .forms import HoistWorkOrderFilterForm
-
-# class HoistWorkOrderListView(ListView):
-#     model = HoistWorkOrder
-#     template_name = 'hoistworkorder_list.html'
-#     context_object_name = 'workorders'
-#     paginate_by = 10
-
-#     def get_queryset(self):
-#         form = HoistWorkOrderFilterForm(self.request.GET)
-
-#         if form.is_valid():
-#             wonumber = form.cleaned_data.get('wonumber')
-#             customer = form.cleaned_data.get('customer')
-#             city = form.cleaned_data.get('city')
-#             capacity = form.cleaned_data.get('capacity')
-#             sales_rep = form.cleaned_data.get('sales_rep')
-#             start_date = form.cleaned_data.get('start_date')
-#             end_date = form.cleaned_data.get('end_date')
-
-#             query = Q()
-
-#             if wonumber:
-#                 query &= Q(Work_Order_Number=wonumber)
-
-#             if customer:
-#                 query &= Q(customer__name__icontains=customer)
-
-#             if city:
-#                 query &= Q(customer__city__icontains=city)
-
-#             if capacity:
-#                 query &= Q(capacity=capacity)
-
-#             if sales_rep:
-#                 query &= Q(sales_rep=sales_rep)
-
-#             if start_date:
-#                 query &= Q(date__gte=start_date)
-
-#             if end_date:
-#                 query &= Q(date__lte=end_date)
-
-#             if query:
-#                 return HoistWorkOrder.objects.filter(query)
-#             else:
-#                 return HoistWorkOrder.objects.all()
-#         else:
-#             return HoistWorkOrder.objects.all()
-        
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['form'] = HoistWorkOrderFilterForm(self.request.GET)
-#         return context
+from .serializers import HoistWorkOrderSerializer
 
 class HoistWorkOrderListView(APIView):
     def get(self, request):
@@ -73,7 +21,7 @@ class HoistWorkOrderListView(APIView):
         query = Q()
 
         if wonumber:
-            query &= Q(Work_Order_Number=wonumber)
+            return redirect('workorder:hwo_detail', hwo=wonumber)
         
         if customer:
             query &= Q(customer__name__icontains=customer)
@@ -104,3 +52,9 @@ class HoistWorkOrderListView(APIView):
             'hwos': hwos,
         }
         return render(request, 'hwo_search_results.html', context)
+    
+class HoistWorkOrderDetailView(APIView):
+    def get(self, request, hwo):
+        workorder = HoistWorkOrder.objects.get(Work_Order_Number=hwo)
+        context = {'hwo':workorder}
+        return render(request, 'hwo_detail.html', context)
